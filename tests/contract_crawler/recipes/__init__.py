7b26d83b939043e3929fa3ac1f6f595e6d77f2fa
quantumNerd <28456871+quantumNerd@users.noreply.github.com>
2026-01-28 23:57:22 -0500

    after golden

--- tests/contract_crawler/recipes/__init__.py
+++ tests/contract_crawler/recipes/__init__.py
@@ -1,6 +1,7 @@
 """Recipes for testing complex RPC methods that require setup."""
 
-from tests.contract_crawler.recipes.structure_flow import GetStepDetailRecipe
+# Use relative import to avoid dependency on tests package structure
+from .structure_flow import GetStepDetailRecipe
 
 # Registry of all recipes for coverage tracking
 ALL_RECIPES = [
